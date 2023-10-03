b7b1f9febd103489dc0ab6ce2aec3b1bc0c9b44f
“Afaf <afaf.taalabi@gmail.com>
2023-10-02 20:05:41 -0700

    mandatory

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,24 +3,6 @@
 #include "main.h"
 
 /**
- * close_file - Closes file descriptors.
- * @fd: The file descriptor to be closed.
- */
-void close_file(int fd)
-{
-	int c;
-
-	c = close(fd);
-
-	if (c == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
-	}
-}
-
-
-/**
  * create_buffer - Allocate 1024 bytes for a buffer
  *
  * @file: The name of the file buffer is storing chars for
@@ -44,6 +26,23 @@ char *create_buffer(char *file)
 }
 
 /**
+ * close_file - Closes file descriptors.
+ * @fd: The file descriptor to be closed.
+ */
+void close_file(int fd)
+{
+        int c;
+
+        c = close(fd);
+
+        if (c == -1)
+        {
+                dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+                exit(100);
+        }
+}
+
+/**
  * main - Copies the contents of a file to another file.
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
@@ -57,7 +56,7 @@ char *create_buffer(char *file)
  */
 int main(int argc, char *argv[])
 {
-	int from, to, r, w;
+	int from, to, r, wr;
 	char *buffer;
 
 	if (argc != 3)
@@ -80,8 +79,8 @@ int main(int argc, char *argv[])
 			exit(98);
 		}
 
-		w = write(to, buffer, r);
-		if (to == -1 || w == -1)
+		wr = write(to, buffer, r);
+		if (to == -1 || wr == -1)
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't write to %s\n", argv[2]);
